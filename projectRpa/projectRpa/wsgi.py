5d65287440be008c30e39038eeefb4806d76075c
jassim <incubator482@gmail.com>
2020-01-07 13:24:32 +0530

    initial commit

--- projectRpa/projectRpa/wsgi.py
+++ projectRpa/projectRpa/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for rpa project.
+WSGI config for projectRpa project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rpa.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projectRpa.settings')
 
 application = get_wsgi_application()
