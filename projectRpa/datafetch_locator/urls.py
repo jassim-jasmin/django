20bbdef9087ef75c51af3e3b0abb09b4447123c4
jassim <incubator482@gmail.com>
2020-01-11 14:02:01 +0530

    url

--- projectRpa/datafetch_locator/urls.py
+++ projectRpa/datafetch_locator/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
+app_name = 'datafetch_locator'
 urlpatterns = [
     path('', views.index, name='index'),
     path('layer/', views.getAllLayer, name='allLayer'),
