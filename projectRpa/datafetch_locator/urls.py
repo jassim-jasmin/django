fc8745ddc5a61690eac3300f8a64c6f099bcedd8
jassim <incubator482@gmail.com>
2020-01-31 17:22:09 +0530

    Locator updated

--- projectRpa/datafetch_locator/urls.py
+++ projectRpa/datafetch_locator/urls.py
@@ -6,6 +6,8 @@ app_name = 'datafetch_locator'
 urlpatterns = [
     path('', views.index, name='index'),
     path('layer_main/', views.layerMain, name='layerMain'),
-    path('<str:layer>/', views.locatorMain, name='locatorMain')
+    path('locator_main/', views.locatorMain, name='locatorMain'),
+    path('add_layer/', views.addLayer, name='add_layer'),
+    path('add_locator/<str:layerName>/', views.addLocator, name='add_locator')
 
 ]
--- projectRpa/datafetch_locator/views.py
+++ projectRpa/datafetch_locator/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render, reverse
 from django.http import Http404
 
 from django.utils import timezone
@@ -14,98 +14,49 @@ def layerMain(request):
 
     return render(request, 'datafetch_locator/layer_main.html', layerJson)
 
-def locatorMain(request, layer):
-    selectedLayer = Layer.objects.get(pk=request.POST['layer'])
+def locatorMain(request):
+    if 'layer' in request.POST:
+        selectedLayer = Layer.objects.get(pk=request.POST['layer'])
 
-    locator = Locator.objects.filter(layer_name=selectedLayer)
+        locator = Locator.objects.filter(layer_name=selectedLayer)
 
-    # locator = layer.objects.locator_set.filter(layer_name=selectedLayer)
-    locatorJson = {'layer' : layer, 'locator' : locator}
-    print(layer)
+        locatorJson = {'locator' : selectedLayer, 'layer_name':selectedLayer}
+    else:
+        return render(request, 'datafetch_locator/locator_main.html', {'locator' : None})
 
     return render(request, 'datafetch_locator/locator_main.html', locatorJson)
 
 def index(request):
     return HttpResponse('hello')
-#     """
-#     todo Need to perform other operation for index page
-#     :param request:
-#     :return:
-#     """
-#     layerArray = MainClass.objects.order_by('-pub_date')[:5]
-#     context = {
-#         'layer_list': layerArray,
-#     }
-#     return render(request, 'datafetch_locator/index.html', context)
-#
-# def getAllLayer(request):
-#     """
-#     Returns all layers available
-#     todo Change to profile wise layer
-#     :param request:
-#     :return: layer list in index.html
-#     """
-#     layerArray = MainClass.objects.order_by('-pub_date')[:5]
-#     context = {
-#         'layer_list': layerArray,
-#     }
-#     return render(request, 'datafetch_locator/index.html', context)
-#
-# def getLocatorId(request, locator_id):
-#     return HttpResponse("Locator id: %s" % locator_id)
-#
-# def layerData(request, layer_id):
-#     """
-#     Purpose of this function is, based on layer_name corresponding locators need to return
-#     :param request:
-#     :param layer_id: layer name
-#     :return: locator coresponding layer
-#     """
-#     try:
-#         getLayer = get_object_or_404(MainClass, layer_name=layer_id)
-#         return render(request, 'datafetch_locator/detail.html', { 'layer_list': getLayer } )
-#     except MainClass.DoesNotExist:
-#         raise Http404("Layer does not exist")
-#
-# def manage_locator(request, locator_id):
-#     print('id', locator_id)
-#
-#     layer = get_object_or_404(MainClass, pk=locator_id)
-#     try:
-#         print('choice;;', request.POST['locator_id'])
-#         layer.locator_set.create(locator_id=request.POST['locator_id'], locator_data=request.POST['locator_data'])
-#         # layer.save()
-#
-#         # selected_choice = layer.locator_set.get(pk=request.POST['locator_select'])
-#         # print('selected choice:',selected_choice)
-#     except (KeyError, MainClass.DoesNotExist):
-#         return render(request, 'datafetch_locator/detail.html', {
-#             'layer_list': layer,
-#             'error_message': "OPERATION FAILED",
-#             'choice':'blah',
-#         })
-#     else:
-#         return render(request, 'datafetch_locator/detail.html', {
-#             'layer_list': layer,
-#     })
-#
-# def addLayer(request, layer_list):
-#     """
-#     For adding new layer
-#     :param request:
-#     :param layerName:
-#     :return:
-#     """
-#     input_layer_name = request.POST['input_layer_name']
-#     print('adding layer', input_layer_name)
-#     print('calling')
-#     layer = MainClass(layer_name=input_layer_name, pub_date=timezone.now())
-#     layer.save()
-#     layer_list = MainClass.objects.order_by
-#
-#     return render(request, 'datafetch_locator/index.html', {
-#             'layer_list': layer_list,
-#     })
+
+def addLayer(request):
+    """
+    For adding new layer
+    :param request:
+    :param layerName:
+    :return:
+    """
+    input_layer_name = request.POST['input_layer_name']
+    print('adding layer', input_layer_name)
+    layer = Layer(layer_name=input_layer_name)
+    layer.save()
+    layer_list = Layer.objects.order_by
+
+    # return HttpResponse('success')
+    return render(request, 'datafetch_locator/layer_main.html', {
+            'layer_name': layer_list,
+    })
+
+def addLocator(request, layerName):
+    if 'add_locator_button' in request.POST:
+        print('layer name', layerName)
+        newLocator = request.POST['add_locator_button']
+        locator = Locator.obje
+
+    return HttpResponse('success')
+
+def testFunction():
+    print('button cliked')
 #
 # def deleteLayer(reques, layer_list):
 #     print('delete layer')
